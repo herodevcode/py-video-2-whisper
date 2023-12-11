8e69239d5fb5b6a7fa250b075604a90d8374f007
herodevcode <134185557+herodevcode@users.noreply.github.com>
2023-12-11 12:36:59 +0000

    fixed error

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from moviepy.editor import VideoFileClip
 import whisper
-
-# Define the path for saving the audio file
-AUDIO_FILE_PATH = "temp_audio.mp3"
+import os
+import tempfile
 
 st.title("Video to Transcription")
 
@@ -11,10 +10,22 @@ st.title("Video to Transcription")
 video_file = st.file_uploader("Upload a video file", type=["mp4", "mov", "avi", "mkv"])
 
 # Function to convert video to audio
-def convert_video_to_audio(video_file):
-    with VideoFileClip(video_file.name) as video:
+def convert_video_to_audio(video_stream):
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmpfile:
+        # Write the uploaded video file to a temporary file
+        tmpfile.write(video_stream.read())
+        tmpfile_path = tmpfile.name
+    
+    # Load the temporary video file with MoviePy and extract audio
+    with VideoFileClip(tmpfile_path) as video:
         audio = video.audio
-        audio.write_audiofile(AUDIO_FILE_PATH)
+        # Define the audio file path
+        audio_file_path = os.path.join(tempfile.gettempdir(), 'temp_audio.mp3')
+        audio.write_audiofile(audio_file_path)
+    # Remove the temporary video file
+    os.unlink(tmpfile_path)
+    return audio_file_path
 
 # Function to transcribe audio using Whisper
 def transcribe_audio(audio_path):
@@ -23,15 +34,15 @@ def transcribe_audio(audio_path):
     return result["text"]
 
 if video_file is not None:
-    # Show a placeholder while processing the file
-    with st.spinner('Converting video to audio...'):
-        convert_video_to_audio(video_file)
+    # Convert video to audio and save the audio file path
+    audio_file_path = convert_video_to_audio(video_file)
 
     with st.spinner('Transcribing audio...'):
-        transcription = transcribe_audio(AUDIO_FILE_PATH)
+        # Transcribe the audio file
+        transcription = transcribe_audio(audio_file_path)
+
+        # Remove the temporary audio file
+        os.unlink(audio_file_path)
 
     # Display the transcription
     st.text_area("Transcription", transcription, height=300)
-
-if __name__ == '__main__':
-    st.run()
